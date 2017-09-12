31028d68bd706126b0a8bed4e731a0a7c159aa4b
saravanakumar.k@zohocorp.com <saravanakumar.k@zohocorp.com>
2017-09-12 09:05:25 +0530

    Error fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
 	#root page
-	root 'welcome/index'
+	root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
